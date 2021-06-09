4ecce45dbb365f17d837db71950aeedf9b77e0fd
Felipe Alexandre <64933218+Felipe-ASouza@users.noreply.github.com>
2021-06-09 10:12:49 -0300

    Primeiro programa feito em python

---
+++ Calculadora.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Calculadora
+Autor: Felipe Alexandre
+Função: Fazer contas: adição, subtração, multiplicação e divisão.
+"""
+
+print("---- Calculadora v2.0 ----")
+
+sair = False
+
+while sair == False:
+
+	num1 = input("Digite o primeiro número: ")
+	num1 = int(num1)
+	operador = input("Digite o operador +-/*: ")
+	num2 = input("Digite o segundo número: ")
+	num2 = int(num2)
+
+	# + adição
+	if operador == "+":
+		operacao = num1 + num2
+
+	# - subtração
+	if operador == "-":
+		operacao = num1 - num2
+
+	# / divisão
+	if operador == "/":
+		operacao = num1 / num2
+
+	# * multiplicação
+	if operador == "*":
+		operacao = num1 * num2
+
+	print("Resultado: ")
+	print(operacao)
+
+	teste = input("Deseja sair n/s: ")
+	if teste == "s":
+		sair = True
